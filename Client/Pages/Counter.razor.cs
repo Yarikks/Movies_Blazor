8ebba7042d956bc24363bb8255f3e9339632b1e1
Yarikks <jasper18poil@gmail.com>
2020-06-10 15:43:46 +0300

    Partial Classes

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Movies_Blazor.Shared.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Movies_Blazor.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+        private List<Movie> movies;
+        private int currentCount = 0;
+
+        protected override void OnInitialized()
+        {
+            movies = new List<Movie>()
+            {
+                new Movie(){Title = "Joker", ReleaseDate = new DateTime(2019, 7, 2) },
+                new Movie(){Title = "Avangers", ReleaseDate = new DateTime(2016, 11, 23) }
+            };
+        }
+        private void IncrementCount()
+        {
+            currentCount++;
+            transient.Value = currentCount;
+            singleton.Value = currentCount;
+        }
+    }
+}
