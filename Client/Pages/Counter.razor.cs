11a14f48ced1af9f347aa90d0a295c2585b1f13a
Yarikks <jasper18poil@gmail.com>
2020-06-11 20:23:37 +0300

    Invoke C# from JS Part 1

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Movies_Blazor.Shared.Entities;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Movies_Blazor.Client.Pages
 {
@@ -9,8 +11,12 @@ namespace Movies_Blazor.Client.Pages
     {
         [Inject] SingletonService singleton { get; set; }
         [Inject] TransientService transient { get; set; }
+        [Inject] IJSRuntime js { get; set; }
+
         private List<Movie> movies;
         private int currentCount = 0;
+        private static int currentCountStatic = 0;
+
 
         protected override void OnInitialized()
         {
@@ -20,11 +26,19 @@ namespace Movies_Blazor.Client.Pages
                 new Movie(){Title = "Avangers", ReleaseDate = new DateTime(2016, 11, 23) }
             };
         }
-        private void IncrementCount()
+        private async Task IncrementCount()
         {
             currentCount++;
+            currentCountStatic++;
             transient.Value = currentCount;
             singleton.Value = currentCount;
+            await js.InvokeVoidAsync("dotnetStaticInvocation");
+        }
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
         }
     }
 }
--- Client/wwwroot/js/Utilities.js
+++ Client/wwwroot/js/Utilities.js
@@ -1,3 +1,10 @@
 ﻿function func(message) {
     console.log("from utilities " + message);
 }
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("Movies_Blazor.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from js " + result);
+        });
+}
