4ae368f5e228ad0654d9e9e7c1d308744455e66b
Yarikks <jasper18poil@gmail.com>
2020-06-13 18:12:49 +0300

    Data Annotations

--- Shared/Entities/Genre.cs
+++ Shared/Entities/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Movies_Blazor.Shared.Entities
@@ -7,6 +8,7 @@ namespace Movies_Blazor.Shared.Entities
     public class Genre
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Name { get; set; }
     }
 }
