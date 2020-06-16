c01069961da0a31955125f55a8f4a3c1e35c2da2
Yarikks <jasper18poil@gmail.com>
2020-06-16 13:30:55 +0300

    Person Form(Edit,Create)

---
+++ Shared/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Movies_Blazor.Shared.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
