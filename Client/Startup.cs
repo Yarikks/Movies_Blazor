8d6c84129ee59c606f599992ea65a28c6c4d33df
Yarikks <jasper18poil@gmail.com>
2020-06-16 13:50:22 +0300

    Inserting Image Component

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -1,3 +1,4 @@
+using Blazor.FileReader;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Movies_Blazor.Client.Helpers;
@@ -9,6 +10,7 @@ namespace Movies_Blazor.Client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IRepository, RepositoryInMemory>();
+            services.AddFileReaderService(options => options.InitializeOnFirstCall = true);
         }
 
         public void Configure(IComponentsApplicationBuilder app)
