ae027de119cd19263e2f09ded6c85f01487405b1
Yarikks <jasper18poil@gmail.com>
2020-06-21 17:22:13 +0300

    Creating Genres

---
+++ Server/Controllers/GenresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies_Blazor.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies_Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController :ControllerBase
+    {
+        private readonly AppDbContext context;
+        public GenresController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Genre genre)
+        {
+            context.Add(genre);
+            await context.SaveChangesAsync();
+            return genre.Id;
+        }
+    }
+}
