842f2e44e78ecba234e5e87db75c7b3487a85403
Yarikks <jasper18poil@gmail.com>
2020-06-21 16:54:26 +0300

    Creating the Database

---
+++ Server/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies_Blazor.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies_Blazor.Server
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoviesActors>().HasKey(ma => new { ma.MovieId, ma.PersonId });
+            modelBuilder.Entity<MoviesGenres>().HasKey(mg => new { mg.MovieId, mg.GenresId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+
+
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,18 +1,28 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Movies_Blazor.Server
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddResponseCompression(opts =>
             {
