0a739b17916d2a841ed60eeb587734a6626a475b
Yarikks <jasper18poil@gmail.com>
2020-07-01 19:06:00 +0300

    Authentication State From C#

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,12 +1,28 @@
-﻿namespace Movies_Blazor.Client.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace Movies_Blazor.Client.Pages
 {
     public partial class Counter
     {
         private int currentCount = 0;
         
-        public void IncrementCount()
+        [CascadingParameter] private Task<AuthenticationState> AuthenticationState { get; set; }
+
+        public async Task IncrementCount()
         {
-            currentCount++;
+            var authState = await AuthenticationState;
+            var user = authState.User;
+
+            if (user.Identity.IsAuthenticated)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount--;
+            }
         }
     }
 }
